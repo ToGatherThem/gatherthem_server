efb391e6b2912e2e673f864b57ed230a6b4593f7
Bryan Prolong <smyllet.gamer@gmail.com>
2022-04-14 13:42:21 +0200

    [FIX] Remove useless import

--- src/main/java/fr/gatherthem/gatherthem_server/collections/infrastructure/repository/CollectionRepository.java
+++ src/main/java/fr/gatherthem/gatherthem_server/collections/infrastructure/repository/CollectionRepository.java
@@ -8,7 +8,6 @@ import fr.gatherthem.gatherthem_server.commons.dao.CollectionDao;
 import fr.gatherthem.gatherthem_server.commons.dao.ItemDao;
 import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
